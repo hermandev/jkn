d89a565e0aad60b2404bf0860135224a4e316ec3
Hermanto Lakoro <codecornn@hermandev.local>
2026-03-24 23:08:18 +0800

    feat: add pcare diagnosa

--- src/pcare/diagnosa.ts
+++ src/pcare/diagnosa.ts
@@ -1,17 +1,31 @@
-import { PCareBaseApi } from "./base.js";
+import { PCareBaseApi } from './base.js';
 
 export class Diagnosa extends PCareBaseApi {
-  async get(params: { kode: string; row: number; limit: number }) {
-    return this.send<{ count: number; list: DiagnosaResult[] }>({
-      name: this.name + "Data Diagnosa",
-      path: ["/diagnosa/:kode/:row/:limit", params],
-      method: "GET",
-    });
-  }
+	async get(params: {
+		/** Kode atau nama diagnosa */
+		kode: string;
+
+		/** Row data awal yang akan ditampilkan */
+		row: number;
+
+		/** Limit jumlah data yang akan ditampilkan */
+		limit: number;
+	}) {
+		return this.send<{ count: number; list: DiagnosaResult[] }>({
+			name: `${this.name} Data Diagnosa`,
+			path: ['/diagnosa/:kode/:row/:limit', params],
+			method: 'GET'
+		});
+	}
 }
 
 interface DiagnosaResult {
-  kdDiag: string;
-  nmDiag: string;
-  nonSpesialis: boolean;
+	/** kode diagnosa */
+	kdDiag: string;
+
+	/** nama diagnonsa */
+	nmDiag: string;
+
+	/** non-spesialis */
+	nonSpesialis: boolean;
 }
--- src/pcare/index.ts
+++ src/pcare/index.ts
@@ -1,14 +1,19 @@
-import { CachedApi } from "../base.js";
+import { CachedApi } from '../base.js';
+import { Diagnosa } from './diagnosa.js';
 
 export class PCare {
-  private static instance: PCare | undefined;
+	private static instance: PCare | undefined;
 
-  private constructor(private readonly cache: CachedApi) {}
+	private constructor(private readonly cache: CachedApi) {}
 
-  static getInstance(cache: CachedApi): PCare {
-    if (!this.instance) {
-      this.instance = new PCare(cache);
-    }
-    return this.instance;
-  }
+	static getInstance(cache: CachedApi): PCare {
+		if (!this.instance) {
+			this.instance = new PCare(cache);
+		}
+		return this.instance;
+	}
+
+	get diagnosa() {
+		return this.cache.get('pcare_diagnosa', Diagnosa);
+	}
 }
