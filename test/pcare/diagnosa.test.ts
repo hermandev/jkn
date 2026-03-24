d9378d0305f451aa0a0ef635eff58ba6e8a13933
Hermanto Lakoro <codecornn@hermandev.local>
2026-03-24 23:09:06 +0800

    feat(test): add file test for pcare diagnosa

---
+++ test/pcare/diagnosa.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+import jkn from '../jkn';
+
+describe('PCare - Diagnnosa', { timeout: 25_000 }, () => {
+	it.concurrent('dataDiagnosa()', async () => {
+		const result = await jkn.pcare.diagnosa.get({ kode: 'A00', row: 1, limit: 10 });
+		console.log(result);
+		expect(result.metaData.code).toBe(200); // no example data
+	});
+});
